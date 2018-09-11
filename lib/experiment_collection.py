5f61beb11b562ff0d3444ff77d427f70459b1620
Sam Minot <sam@fredhutch.org>
2018-09-11 14:38:14 -0700

    Refactor to cache at the sample level, not the entire experiment

--- lib/experiment_collection.py
+++ lib/experiment_collection.py
@@ -30,8 +30,7 @@ class ExperimentCollection:
                 if sample_id.startswith("/abundance/")
             ]
 
-    @lru_cache(maxsize=2)
-    def gene_abundance(self, metric=None):
+    def gene_abundance(self, genes=None, samples=None, metric=None):
         """
         
         Return a DataFrame with the abundance for a set of samples.
@@ -40,10 +39,15 @@ class ExperimentCollection:
         Another option would be "clr".
 
         """
-        
-        # Open a connection to the HDFStore
-        store = pd.HDFStore(self.exp_col_fp, mode="r")
 
+        # Default to returning all samples
+        if samples is None:
+            samples = self.all_samples
+        else:
+            assert isinstance(samples, list)
+            for sample_id in samples:
+                assert sample_id in self.all_samples, "{} is not a valid sample".format(sample_id)
+        
         # Set the metric to return
         if metric is None:
             metric = self.abund_id_key
@@ -52,23 +56,21 @@ class ExperimentCollection:
         df = {}
 
         # Iterate over the samples
-        for sample_id in self.all_samples:
+        for sample_id in samples:
             # Read the abundance
-            abund = pd.read_hdf(store, "/abundance/" + sample_id)
-
-            for k in [self.gene_id_key, metric]:
-                assert k in abund.columns.values, "Column {} not found for {}".format(k, sample_id)
-
-            # Add to the dict
-            df[sample_id] = abund.set_index(self.gene_id_key)[metric]
-
-        # Close the connection to the HDF5 file
-        store.close()
+            df[sample_id] = self.sample_gene_abundance(
+                sample_id,
+                metric=metric
+            )
+            
+            # Subset to the genes of interest
+            if genes is not None:
+                df[sample_id] = df[sample_id].reindex(genes)
 
         # Format as a DataFrame
         return pd.DataFrame(df)
 
-    @lru_cache(maxsize=128)
+    @lru_cache(maxsize=512)
     def sample_gene_abundance(self, sample_id, metric=None):
         """
         
@@ -92,7 +94,7 @@ class ExperimentCollection:
 
         return abund.set_index(self.gene_id_key)[metric]
 
-    @lru_cache(maxsize=128)
+    @lru_cache(maxsize=512)
     def sample_cag_abundance(self, sample_id, metric=None):
         """
         
@@ -149,8 +151,7 @@ class ExperimentCollection:
             "taxonomic_classification"
         ).set_index("gene")
 
-    @lru_cache(maxsize=2)
-    def cag_abundance(self, metric=None):
+    def cag_abundance(self, cags=None, samples=None, metric=None):
         """
         
         Return a DataFrame with the abundance of CAGs for a set of samples.
@@ -160,8 +161,8 @@ class ExperimentCollection:
 
         """
 
-        # Open a connection to the HDFStore
-        store = pd.HDFStore(self.exp_col_fp, mode="r")
+        if samples is None:
+            samples = self.all_samples
 
         # Set the metric to return
         if metric is None:
@@ -171,19 +172,12 @@ class ExperimentCollection:
         df = {}
 
         # Iterate over the samples
-        for sample_id in self.all_samples:
+        for sample_id in samples:
             # Read the abundance
-            abund = pd.read_hdf(store, "/cag_abundance/" + sample_id)
-            
-            for k in ["cag_id", metric]:
-                assert k in abund.columns.values, "Column {} not found for {}".format(
-                    k, sample_id)
+            df[sample_id] = self.sample_cag_abundance(sample_id, metric=metric)
 
-            # Add to the dict
-            df[sample_id] = abund.set_index("cag_id")[metric]
-
-        # Close the connection to the HDF5 file
-        store.close()
+            if cags is not None:
+                df[sample_id] = df[sample_id].reindex(cags)
 
         # Format as a DataFrame
         return pd.DataFrame(df)
